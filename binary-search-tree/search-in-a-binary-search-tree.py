ebd9e24fb9fe20897ba20e3de52c17c49042a394
Jeevs <110006237+jeevssx@users.noreply.github.com>
2024-11-17 23:33:45 -0500

    LC-search-in-a-binary-search-tree [Runtime: 0 ms; Memory: 18.6 MB]

--- binary-search-tree/search-in-a-binary-search-tree.py
+++ binary-search-tree/search-in-a-binary-search-tree.py
@@ -6,12 +6,20 @@
 #         self.right = right
 class Solution:
     def searchBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        
         if not root:
             return None
+
         
-        if val < root.val:
+
+        if root.val > val:
+
             return self.searchBST(root.left, val)
-        elif val > root.val:
+
+        if root.val < val:
+
             return self.searchBST(root.right, val)
         else:
+ 
             return root
+        
